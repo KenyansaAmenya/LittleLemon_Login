08a18ffc9cc63328bad02a4b4edc92ab44451a80
Felix Amenya <kenyansafelix@gmail.com>
2024-01-31 23:32:38 +0300

    test

--- app/src/main/java/com/example/littlelemonlogin/MainActivity.kt
+++ app/src/main/java/com/example/littlelemonlogin/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,6 +17,7 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.littlelemonlogin.ui.theme.LittleLemonLoginTheme
@@ -43,9 +46,23 @@ fun LoginScreen() {
     ){
         Image(painter = painterResource(id = R.drawable.littlelemonlogo), contentDescription = "Logo Image"
         )
-        TextField(value = "", onValueChange = {},
+        TextField(value = "",
+            onValueChange = {},
             label = { Text(text = "Username")},
         )
+        TextField(value = "",
+            onValueChange = {},
+            label = { Text(text = "Password")}
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                Color(0xFF495E57))
+        ) {
+            Text(text = "Login",
+                color = Color(0xFFEDEFEE)
+            )
+        }
     }
 }
 
